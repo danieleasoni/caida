af1469488d28e829bc63c31fb91f92fcd01202ad
Daniele Asoni <dasoni@inf.ethz.ch>
2015-11-10 19:12:46 +0100

    Initial (dirty) commit for flow labelling script

---
+++ get_flows.cpp
@@ -0,0 +1,100 @@
+/*
+ *  Original program by Tony Lukasavage
+ *  Taken from: http://tonylukasavage.com/blog/2010/12/19/offline-packet-capture-analysis-with-c-c----amp--libpcap/
+ *  date: 09/11/2015 (original post: 19/12/2010)
+ */
+
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <pcap.h>
+#include <map>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+map<string,long> flows;
+unsigned long counter = 0;
+
+void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet);
+
+int main(int argc, char *argv[]) {
+  pcap_t *descr;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  const char *in_file = "";
+
+  if (argc != 2) {
+      cerr << "Expecting 1 command line argument (input pcap file)" << endl;
+      return 1;
+  }
+
+  in_file = argv[1];
+
+  // open capture file for offline processing
+  descr = pcap_open_offline(in_file, errbuf);
+  if (descr == NULL) {
+      cerr << "pcap_open_live() failed: " << errbuf << endl;
+      return 1;
+  }
+
+  // start packet processing loop, just like live capture
+  if (pcap_loop(descr, 0, packetHandler, NULL) < 0) {
+      cerr << "pcap_loop() failed: " << pcap_geterr(descr) << endl;
+      return 1;
+  }
+
+  return 0;
+}
+
+void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
+  const struct ip* ipHeader;
+  const struct tcphdr* tcpHeader;
+  const struct udphdr* udpHeader;
+  char sourceIp[INET_ADDRSTRLEN];
+  char destIp[INET_ADDRSTRLEN];
+  u_int sourcePort = 0, destPort = 0;
+  bool fwd;
+  int data_length = 0;
+  stringstream sstm;
+  string fivetuple;
+
+  ipHeader = (struct ip*)packet;
+  inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIp, INET_ADDRSTRLEN);
+  inet_ntop(AF_INET, &(ipHeader->ip_dst), destIp, INET_ADDRSTRLEN);
+
+  if (ipHeader->ip_p == IPPROTO_TCP) {
+      tcpHeader = (tcphdr*)(packet + sizeof(struct ip));
+      sourcePort = ntohs(tcpHeader->source);
+      destPort = ntohs(tcpHeader->dest);
+  }
+
+  if (ipHeader->ip_p == IPPROTO_UDP) {
+      udpHeader = (udphdr*)(packet + sizeof(struct ip));
+      sourcePort = ntohs(udpHeader->source);
+      destPort = ntohs(udpHeader->dest);
+  }
+
+  fwd = (strcmp(sourceIp,destIp) < 0)
+
+  if (fwd)
+      sstm << str(sourceIp) << "#" << str(destIp) << "#" << (int)ipHeader->ip_p << "#" << sourcePort << "#" << destPort;
+  else
+      sstm << str(destIp) << "#" << str(sourceIp) << "#" << (int)ipHeader->ip_p << "#" << destPort << "#" << sourcePort;
+  fivetuple = sstm.str();
+
+  
+
+  if (fwd)
+      cout << sourceIp << "\t" << destIp << "\t" << (int)ipHeader->ip_p << "\t" << sourcePort << "\t" << destPort << "\t>\t";
+  else
+      cout << destIp << "\t" << sourceIp << "\t" << (int)ipHeader->ip_p << "\t" << destPort << "\t" << sourcePort << "\t<\t";
+
+  data_length = pkthdr->len;
+  cout << data_length << "\t" << pkthdr->ts.tv_sec << "\t" << pkthdr->ts.tv_usec << endl;
+
+}
