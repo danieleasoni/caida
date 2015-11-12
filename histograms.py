7621d62ffb665a2e92ae97fa9692c8b5010d84f8
Daniele Asoni <dasoni@inf.ethz.ch>
2015-11-12 13:16:01 +0100

    Improved histogram generation script

--- histograms.py
+++ histograms.py
@@ -7,29 +7,32 @@ import time
 
 USAGE = "Usage: python histograms.py infile"
 
+BUCKETS = [0,5,10,15,20,25,30,35,40,45,50]
+
 def get_new_filename(base, ext=""):
     return str(base) + "_" + time.strftime("%Y%m%d-%H%M%S") + str(ext)
 
-def plot_histogram(data_file, delimiter=None, col_num=0):
+def plot_histogram(data_file, delimiter=None, converters=None, col_num=0):
     assert type(col_num) is IntType, "col_num is not an integer: %r" % col_num
-    v = NP.loadtxt(data_file, delimiter=delimiter, usecols=(col_num,))
-    fig = plt.hist(v)
-    plt.savefig(get_new_filename("figs/distribution", "eps"))
-    plt.savefig(get_new_filename("figs/distribution", "pdf"))
-    fig = plt.hist(v, cumulative=True, histtype='step')
-    plt.savefig(get_new_filename("figs/distribution", "eps"))
-    plt.savefig(get_new_filename("figs/distribution", "pdf"))
+    v = NP.loadtxt(data_file, delimiter=delimiter, converters=converters, usecols=(col_num,))
+    fig = plt.hist(v, bins=BUCKETS)
+    plt.savefig(get_new_filename("figs/distribution", ".eps"))
+    plt.savefig(get_new_filename("figs/distribution", ".pdf"))
+    fig = plt.hist(v, cumulative=True, histtype='step', bins=BUCKETS)
+    plt.savefig(get_new_filename("figs/distribution", ".eps"))
+    plt.savefig(get_new_filename("figs/distribution", ".pdf"))
     plt.show()
 
 COL_NUM=3
 
 if __name__ == "__main__":
+    converters = {COL_NUM : lambda s: float(s if s != 'inf' else -1)}
     if len(sys.argv) < 2:
-        plot_histogram(sys.stdin, col_num=COL_NUM)
+        plot_histogram(sys.stdin, converters=converters, col_num=COL_NUM)
     else:
         try:
             with open(sys.argv[1], 'r') as data_file:
-                plot_histogram(data_file, col_num=COL_NUM)
+                plot_histogram(data_file, converters=converters, col_num=COL_NUM)
         except IOError as e:
             print("Unable to open file " + sys.argv[1], file=sys.stderr)
             print(USAGE, file=sys.stderr)
