c4b7207e6459f0d2e14217aaf1e031e25cc413fb
Daniele Asoni <dasoni@inf.ethz.ch>
2015-11-12 13:28:55 +0100

    Fixes to histograms.py script
    Create figs directory if it doesn't exist
    I also removed the plt.show(), so the script can run in the background

--- histograms.py
+++ histograms.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import sys
+import sys, os
 import matplotlib.pyplot as plt
 import numpy as NP
 from types import *
@@ -16,12 +16,13 @@ def plot_histogram(data_file, delimiter=None, converters=None, col_num=0):
     assert type(col_num) is IntType, "col_num is not an integer: %r" % col_num
     v = NP.loadtxt(data_file, delimiter=delimiter, converters=converters, usecols=(col_num,))
     fig = plt.hist(v, bins=BUCKETS)
+    if not os.path.exists("figs"):
+        os.makedirs("figs")
     plt.savefig(get_new_filename("figs/distribution", ".eps"))
     plt.savefig(get_new_filename("figs/distribution", ".pdf"))
     fig = plt.hist(v, cumulative=True, histtype='step', bins=BUCKETS)
     plt.savefig(get_new_filename("figs/distribution", ".eps"))
     plt.savefig(get_new_filename("figs/distribution", ".pdf"))
-    plt.show()
 
 COL_NUM=3
 
@@ -34,7 +35,8 @@ if __name__ == "__main__":
             with open(sys.argv[1], 'r') as data_file:
                 plot_histogram(data_file, converters=converters, col_num=COL_NUM)
         except IOError as e:
-            print("Unable to open file " + sys.argv[1], file=sys.stderr)
+            print("Unable to open file", file=sys.stderr)
+            print(str(e), file=sys.stderr)
             print(USAGE, file=sys.stderr)
             sys.exit(1)
 
