66d7b32ec53439ee7d8e1c1391c2ec2375836b01
Daniele Asoni <dasoni@inf.ethz.ch>
2015-11-19 17:30:05 +0100

    Improvements to histograms.py, added bytes-per-sec

--- histograms.py
+++ histograms.py
@@ -14,18 +14,19 @@ CONVERTERS = {3 : lambda s: float(s if s != 'inf' else -1),
 
 BANDWIDTH_PPS_BINS = range(0, 51, 5)
 
-def get_new_filename(base, ext=""):
-    base_time = str(base) + "_" + time.strftime("%Y%m%d-%H%M%S")
-    current_base = base_time
+def get_new_filename(base, ext="", with_time=False):
+    if with_time:
+        base = str(base) + "_" + time.strftime("%Y%m%d-%H%M%S")
+    current_base = base
     counter = 1
     while os.path.exists(current_base + ext):
-        current_base = base_time + "_" + str(counter)
+        current_base = base + "_" + str(counter)
         counter += 1
     return current_base + ext
 
-def get_column_data(data_file, delimiter=None, converters=None, col_num=0):
-    assert type(col_num) is IntType, "col_num is not an integer: %r" % col_num
-    return NP.loadtxt(data_file, delimiter=delimiter, converters=converters, usecols=(col_num,))
+#def get_column_data(data_file, delimiter=None, converters=None, col_num=0):
+#    assert type(col_num) is IntType, "col_num is not an integer: %r" % col_num
+#    return NP.loadtxt(data_file, delimiter=delimiter, converters=converters, usecols=(col_num,))
 
 def generate_hist_and_save(x, filename, generate_pdf=False, **kwargs):
     fig = plt.hist(x, **kwargs)
@@ -33,19 +34,21 @@ def generate_hist_and_save(x, filename, generate_pdf=False, **kwargs):
     if generate_pdf:
         plt.savefig(os.path.splitext(filename)[0] + ".pdf")
     plt.close()
-#    plt.hist(x, histtype='step', bins=BUCKETS)
-#    plt.savefig(get_new_filename("figs/distribution", ".eps"))
-#    plt.savefig(get_new_filename("figs/distribution", ".pdf"))
-#    plt.hist(x, histtype='step', normed=True, cumulative=True, bins=BUCKETS)
-#    plt.savefig(get_new_filename("figs/distribution", ".eps"))
-#    plt.savefig(get_new_filename("figs/distribution", ".pdf"))
 
-def generate_bandwidth_pps_histograms(data_file):
-    x = get_column_data(data_file, col_num=3)
+def generate_bandwidth_pps_histograms(data_array):
+    x = data_array[:,2]
     filename = get_new_filename(os.path.join(OUTDIR, "bwidth_pps"), ".eps")
     generate_hist_and_save(x, filename, bins=BANDWIDTH_PPS_BINS)
     filename = get_new_filename(os.path.join(OUTDIR, "bwidth_pps_cumul"), ".eps")
     generate_hist_and_save(x, filename, bins=BANDWIDTH_PPS_BINS,
+                           cumulative=True, histtype='step', normed=True)
+
+def generate_bandwidth_bps_histograms(data_array):
+    x = data_array[:,3]
+    filename = get_new_filename(os.path.join(OUTDIR, "bwidth_bps"), ".eps")
+    generate_hist_and_save(x, filename)
+    filename = get_new_filename(os.path.join(OUTDIR, "bwidth_bps_cumul"), ".eps")
+    generate_hist_and_save(x, filename,
                            cumulative=True, histtype='step', normed=True)
 
 if __name__ == "__main__":
@@ -63,8 +66,13 @@ if __name__ == "__main__":
             print(USAGE, file=sys.stderr)
             sys.exit(1)
 
+    # Load data from file into an array
+    data_array = NP.loadtxt(data_file)
+
     # Bandwidth in packets-per-second histogram
-    generate_bandwidth_pps_histograms(data_file)
+    generate_bandwidth_pps_histograms(data_array)
+    # Bandwidth in bytes-per-second histogram
+    generate_bandwidth_bps_histograms(data_array)
 
     if data_file is not sys.stdin:
         data_file.close()
