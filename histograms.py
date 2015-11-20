0612720372c28ea6ba799628654d537c2eea4371
Daniele Asoni <dasoni@inf.ethz.ch>
2015-11-20 19:12:19 +0100

    Updated script, adding scatter plot generation

--- histograms.py
+++ histograms.py
@@ -14,6 +14,8 @@ CONVERTERS = {3 : lambda s: float(s if s != 'inf' else -1),
 
 BANDWIDTH_PPS_BINS = NP.linspace(0, 20, num=100)
 BANDWIDTH_BPS_BINS = NP.linspace(0, 20, num=100)
+LIFETIME_BINS = NP.linspace(0, 20, num=100)
+SCATTER_SAMPLE_SIZE = 10000 
 
 def get_new_filename(base, ext="", with_time=False):
     if with_time:
@@ -29,28 +31,34 @@ def get_new_filename(base, ext="", with_time=False):
 #    assert type(col_num) is IntType, "col_num is not an integer: %r" % col_num
 #    return NP.loadtxt(data_file, delimiter=delimiter, converters=converters, usecols=(col_num,))
 
-def generate_hist_and_save(x, filename, generate_pdf=False, **kwargs):
-    fig = plt.hist(x, **kwargs)
+def generate_hist_and_save(x, filename, bins=None, generate_pdf=False):
+    # Generate and save normal histograms
+    filename = get_new_filename(os.path.join(OUTDIR, filename), ".eps")
+    fig = plt.hist(x, bins=bins)
+    plt.savefig(filename)
+    if generate_pdf:
+        plt.savefig(os.path.splitext(filename)[0] + ".pdf")
+    plt.close()
+    # Generate and save cumulative distribution
+    filename = os.path.splitext(filename)[0] + "_cumul.eps"
+    fig = plt.hist(x, bins=bins, cumulative=True, histtype='step', normed=True)
+    ax = plt.gca()
+    ax.set_ylim([ax.get_ylim()[0], 1.0]) #Set maximum to 1 for normed y-axis (cumulative distrib)
     plt.savefig(filename)
     if generate_pdf:
         plt.savefig(os.path.splitext(filename)[0] + ".pdf")
     plt.close()
 
-def generate_bandwidth_pps_histograms(data_array):
-    x = data_array[:,2]
-    filename = get_new_filename(os.path.join(OUTDIR, "bwidth_pps"), ".eps")
-    generate_hist_and_save(x, filename, bins=BANDWIDTH_PPS_BINS)
-    filename = get_new_filename(os.path.join(OUTDIR, "bwidth_pps_cumul"), ".eps")
-    generate_hist_and_save(x, filename, bins=BANDWIDTH_PPS_BINS,
-                           cumulative=True, histtype='step', normed=True)
-
-def generate_bandwidth_bps_histograms(data_array):
-    x = data_array[:,3]
-    filename = get_new_filename(os.path.join(OUTDIR, "bwidth_bps"), ".eps")
-    generate_hist_and_save(x, filename, bins=BANDWIDTH_BPS_BINS)
-    filename = get_new_filename(os.path.join(OUTDIR, "bwidth_bps_cumul"), ".eps")
-    generate_hist_and_save(x, filename, bins=BANDWIDTH_BPS_BINS,
-                           cumulative=True, histtype='step', normed=True)
+def generate_lifetime_bandwidth_scatter(lifetime, bandwidth, num_samples, generate_pdf=False):
+    assert len(lifetime) == len(bandwidth)
+    filename = get_new_filename(os.path.join(OUTDIR, "lifetime_vs_bw"), ".eps")
+    idx = NP.random.choice(len(lifetime), num_samples)
+    fig = plt.scatter(lifetime[idx], bandwidth[idx])
+    fig = plt.hexbin(lifetime[idx], bandwidth[idx])
+    plt.savefig(filename)
+    if generate_pdf:
+        plt.savefig(os.path.splitext(filename)[0] + ".pdf")
+    plt.close()
 
 if __name__ == "__main__":
     if not os.path.exists(OUTDIR):
@@ -70,10 +78,14 @@ if __name__ == "__main__":
     # Load data from file into an array
     data_array = NP.loadtxt(data_file)
 
+    # Session lifetime in seconds histogram
+    #generate_hist_and_save(data_array[:,2], "lifetime_sec", bins=LIFETIME_BINS)
     # Bandwidth in packets-per-second histogram
-    generate_bandwidth_pps_histograms(data_array)
+    #generate_hist_and_save(data_array[:,3], "bwidth_pps", bins=BANDWIDTH_PPS_BINS)
     # Bandwidth in bytes-per-second histogram
-    generate_bandwidth_bps_histograms(data_array)
+    #generate_hist_and_save(data_array[:,4], "bwidth_bps", bins=BANDWIDTH_BPS_BINS)
+    # Scatter plot of lifetime vs bandwidth
+    generate_lifetime_bandwidth_scatter(data_array[:,2], data_array[:,3], SCATTER_SAMPLE_SIZE)
 
     if data_file is not sys.stdin:
         data_file.close()
