5babfbfc05d01756c3105f4895f185175d24bd2a
Daniele Asoni <dasoni@inf.ethz.ch>
2015-12-11 15:40:36 +0100

    Timeval difference calculation in utils

--- utils.cpp
+++ utils.cpp
@@ -4,6 +4,16 @@
 #include <unistd.h>
 #include <ctime>
 
+void timeval_sub(struct timeval* beginning, struct timeval *end,
+                 struct timeval* res) {
+    res->tv_sec = end->tv_sec - beginning->tv_sec;
+    res->tv_usec = end->tv_usec - beginning->tv_usec;
+    if (res->tv_usec < 0) {
+        res->tv_usec += 1000000;
+        res->tv_sec -= 1;
+    }
+}
+
 std::string get_new_filename(std::string base_name, std::string ext) {
     std::string file_name = base_name + ext;
     unsigned int counter = 1;
--- utils.h
+++ utils.h
@@ -5,14 +5,27 @@
 #include <unistd.h>
 #include <ctime>
 
+// Convert a timeval structure to seconds (float)
 inline double timeval_to_seconds(struct timeval *tv) {
   return tv->tv_sec + double(tv->tv_usec)/1000000;
 }
+
+// Subtracts the time value in end from the time value in end from the time
+// value in beginning, and places the difference in the timeval pointed to
+// by res. The result is normalized such that res->tv_usec has a value in the
+// range 0 to 999,999.
+//
+// This definition is taken from the timersub manpage (bsd).
+void timeval_sub(struct timeval* beginning, struct timeval *end,
+                 struct timeval* res);
 
 inline bool file_exists(const std::string& name) {
     return ( access ( name.c_str(), F_OK ) != -1 );
 }
 
+// Returns a filename (with given extension, if any) that *probably* does not
+// exist. Note that this function performs no locking and is therefore
+// NOT THREAD SAFE!
 std::string get_new_filename(std::string base_name, std::string ext = "");
 
 #endif // NETSEC_UTILS_H_
